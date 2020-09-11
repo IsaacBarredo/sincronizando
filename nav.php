05be4865a8ca80d0b512599cd8c4e7919fe5eeff
Pnk9 <71128090+Pnk9@users.noreply.github.com>
2020-09-11 12:45:24 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
   <ol>
     <li><a href="#">Home</a></li>
     <li><a href="#">Quienes somos</a></li>
-    <li><a href="#">Nosotros</a><li>
+    <li><a href="#">Nosotros</a></li>
     <li><a href="#">Contacto</a></li>
   </ol>
 </header>
