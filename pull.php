893ff1e24ad4f4507b90e5629c32558438d689d8
Pnk9 <71128090+Pnk9@users.noreply.github.com>
2020-09-11 13:13:40 +0200

    actualizando el archivo php

---
+++ pull.php
@@ -0,0 +1,13 @@
+<?php 
+
+  $persona = [
+      "nombre" => "JoseAntonio",
+      "apellido" => "Garrido",
+      "edad" => "50"
+  ];
+
+  foreach ($ersona as $valor) {
+    echo 'valor <br>';
+  }
+
+?>
